515cb95db96275acd25260c5d7fc65856c130dc6
Michael <michael.robison168@gmail.com>
2016-11-12 07:55:25 -0700

    fixed app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
 import { MaterialModule } from '@angular/material'
-import { RouterModule }   from '@angular/router'
 
 import { AUTH_PROVIDERS } from 'angular2-jwt'
 import { ChartModule } from 'angular2-highcharts'
@@ -12,23 +11,18 @@ import { AuthHttp } from 'angular2-jwt'
 
 import { AppComponent } from './app.component'
 import { BackendService } from './backend.service'
-import { GeofenceService, MapComponent } from './map'
-import { ChartComponent, ChartService } from './chart'
-import { ToggleComponent, ToggleService } from './toggle'
-import { BrokerComponent, BrokerService } from './broker'
+import { GeofenceService } from './map'
+import { ChartService } from './chart'
+import { ToggleService } from './toggle'
+import { BrokerService } from './broker'
 import { AuthService } from './auth.service'
 import { AuthGuard } from './auth-guard.service'
-import { routing } from './app.routing'
-import { HomeComponent } from './home'
+import { routing, routedComponents } from './app.routing'
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrokerComponent,
-    ChartComponent,
-    MapComponent,
-    ToggleComponent,
-    HomeComponent,
+    routedComponents
   ],
   imports: [
     AgmCoreModule.forRoot({apiKey: 'AIzaSyDb-Foka_83ay6ofqqwuB33F_p11vtlBjY'}),

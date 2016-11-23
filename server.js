5d574ab4894fcdc55138615e67b5023ad3ebf152
Michael <michael.robison168@gmail.com>
2016-11-23 14:01:15 -0700

    fixed old jwt schema

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ mqtt.on('connect', () => {
               res.json({ success: false, message: 'Authentication failed. Wrong password' })
             } else {
               const token = jwt.sign(user, app.get('superSecret'), {
-                expiresInMinutes: 1440
+                expiresIn: '7d'
               })
               res.json({
                 success: true,
